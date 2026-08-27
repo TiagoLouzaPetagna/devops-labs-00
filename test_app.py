732b7f338d9c4da04523b3d9d54e1287082f4fef
Tiago Louzã <rm562404@fiap.com.br>
2026-08-27 11:35:27 +0000

    feat: Adiciona a função multiplicação

--- test_app.py
+++ test_app.py
@@ -4,6 +4,11 @@ def test_soma():
     assert soma(2, 3) == 5
     assert soma(-1, 1) == 0
 
+from app import soma, saudacao, multiplicacao
+
+def test_multiplicacao():
+    assert multiplicacao(2, 3) == 6
+
 def test_saudacao():
     resultado = saudacao("Mundo")
     assert "Mundo" in resultado
